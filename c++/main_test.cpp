8fc82645a4a732dd468ed005a453ffc8678b9e66
Jérôme Leducq <jleducq@iMac-de-Jerome.local>
2015-04-17 23:24:55 -0400

    remove useless files

--- c++/main_test.cpp
+++ c++/main_test.cpp
@@ -1,11 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
-int main(void)
-{
-	cout<<"Main Test"<<endl;
-	return EXIT_SUCCESS;
+
+int main() {
+    int nbCases;
+    cin>>nbCases;
+    
+    while (nbCases--) {
+        string myString;
+        cin>>myString;
+
+        int i;
+        for (i = myString.size()-1; i > 0 ;--i)
+        {           
+            if (myString[i] > myString[i-1]) {
+                myString.push_back(myString[i-1]);
+                myString.erase(i-1, 1);
+                break;
+            }
+        }
+        
+        if (i==0) cout<<"no answer"<<endl;
+        else cout<<myString<<endl;
+    }
+    return 0;
 }
+
